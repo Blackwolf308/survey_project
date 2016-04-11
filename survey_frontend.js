5ba6b401b239ff5855fa77be819bdbd7b4b5fa81
Blackwolf308 <blackwolf3012@live.com>
2016-04-11 16:09:17 -0400

    survey_project

--- survey_frontend.js
+++ survey_frontend.js
@@ -4,8 +4,20 @@ $(document).ready(function() {
         method: "GET",
         success: function(res){
             var courses = $.parseJSON(res);
+            c=courses;
             for (var i=0; i<courses.length; i++) {
-                $("select#course").append("<option value='" + courses[i].c_ID + "'>" + courses[i].course_code + " " + courses[i].description + " " + courses[i].professor + "</option>");
+                $("select.courses").append("<option value='" + courses[i].c_ID + "'>" + courses[i].course_code + " " + courses[i].start_date + " " + courses[i].professor + "</option>");
+            }
+        }
+    });
+    $.ajax({
+        url: "http://localhost:9000/get-questions",
+        method: "GET",
+        success: function(resp){
+            var questions = $.parseJSON(resp);
+            r=questions;
+            for (var i=0; i<questions.length; i++) {
+                $("select.questions1").append("<option value='" + questions[0].q_id + "'>" + " " + questions[0].resp1 + "</option>");
             }
         }
     });
--- survey_project.js
+++ survey_project.js
@@ -3,25 +3,26 @@ var port    = 9000,
     app     = express();
 
 var mysql = require('mysql'),
-    survey_app = mysql.createConnection ({
+    survey_project = mysql.createConnection ({
         host: 'localhost',
         user: 'root',
         password: '',
-        database: 'survey_app'
+        database: 'survey_project'
     });
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     next();
 });
-
+/*.get will run if we get a "get" request to this URL.
+you could use this whole get request as a POST request. Just a few minor word changes.
+*/
 app.get('/get-questions', function(req, res) {
     var myQuery = "SELECT * FROM questions";
-    survey_app.query(myQuery, function(err, rows) {
+    survey_project.query(myQuery, function(err, rows) {
         if (err) {
             console.log(err);
         } else {
-            console.log('success');
             res.end(JSON.stringify(rows));
         };
     });
@@ -29,7 +30,7 @@ app.get('/get-questions', function(req, res) {
 
 app.get('/get-courses', function(req, res) {
     var myQuery = "SELECT * FROM courses";
-    survey_app.query(myQuery, function(err, rows) {
+    survey_project.query(myQuery, function(err, rows) {
         if (err) {
             console.log(err);
         } else {
