594e045a38152c42a607e8e3bd73e81acd404934
Blackwolf308 <blackwolf3012@live.com>
2016-04-15 14:28:34 -0400

    updated version

--- survey_frontend.js
+++ survey_frontend.js
@@ -4,9 +4,23 @@ $(document).ready(function() {
         method: "GET",
         success: function(res){
             var courses = $.parseJSON(res);
-            c=courses;
-            for (var i=0; i<courses.length; i++) {
-                $("select.courses").append("<option value='" + courses[i].c_ID + "'>" + courses[i].course_code + " " + courses[i].start_date + " " + courses[i].professor + "</option>");
+            $("#survey-parts").prepend(`
+<div class='well'>
+    <div class='row'>
+        <div class="col-xs-10 col-xs-offset-1 col-md-8 col-md-offset-0">
+            <h3 class="text-center">What course ar you taking?</h3>
+        </div>
+        <div class="col-xs-10 col-xs-offset-1 col-md-4 col-md-offset-0">
+            <select class="form-control input-lg"name="c-id">
+                <option disabled>Select One</option>
+            </select>
+        </div>
+    </div>
+</div>`);
+            for (var i=0; i < courses.length; i++) {
+                $("part0 select").append(`<option value="${courses[i].c_ID}>
+                        ${courses[i].courses_code.toUpperCase()} taught by ${courses[i].professor}
+                            </option>`);
             }
         }
     });
