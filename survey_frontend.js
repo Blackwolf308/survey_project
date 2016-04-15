715bf05b6c3d2c91e68d183b0e9033f5f8c990df
Blackwolf308 <blackwolf3012@live.com>
2016-04-15 14:54:09 -0400

    updated_new

--- survey_frontend.js
+++ survey_frontend.js
@@ -31,8 +31,61 @@ $(document).ready(function() {
             var questions = $.parseJSON(resp);
             r=questions;
             for (var i=0; i<questions.length; i++) {
-                $("select.questions1").append("<option value='" + questions[0].q_id + "'>" + " " + questions[0].resp1 + "</option>");
+                var questions =$(`
+<div>
+    <div class='row'>
+        <div class="col-xs-10 col-xs-offset-1 col-md-8 col-md-offset-0>
+            <h3 class="text_center">${questions[i].questions}</h3>
+        </div>
+        <div class="col-xs-10 col-xs-offset-1 col-md-4 col-md-offset-0>
+            <div cass'form-group' id='part${i+1}'>
+                <select class="form-control input-lg" name="q${i + 1}">
+                    <option disabled >Select One</option>
+                </select>
+            </div>
+        </div>
+    </div>
+</div>`);
+                $("#survey-parts").append(question);
+                for (var x=1; x <= 5; x++) {
+                    $(("#part" + (i+1)) + " select").append(
+                        `<option value="${x}">
+${questions[i]["R" + x]}</option>`);
+                }
             }
         }
     });
+    $(".formSubmit").click(function(e) {
+        e.preventDefault();
+        $.ajax({
+            url:"http://localhost:9000/get-responses" + $("survey").serialize(),
+            method: "POST",
+            success: function (res) {
+                alert("Thank you for your time");
+            }
+        })
+    })
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
